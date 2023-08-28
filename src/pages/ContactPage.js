d7ea72a66287b10484ee14b6ce3faf3936f8d0c8
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-08-28 16:53:43 +0600

    contactpage added

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactPage() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            {/* Your about page content */}
+        </div>
+    );
+}
+
+export default ContactPage;
