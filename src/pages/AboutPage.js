1a2a941d7de6a1e8261f1f15a0101263481724bf
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-08-28 16:53:49 +0600

    about page added

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+    return (
+        <div>
+            <h2>About Us</h2>
+            {/* Your about page content */}
+        </div>
+    );
+} 
+
+export default AboutPage;
